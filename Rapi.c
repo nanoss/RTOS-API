09935991eae7590882da4ecdd22cf0c7246c26e4
nanoss <nanos@qq.com>
2017-11-25 19:53:54 +0800

    rename file name

--- Rapi.c
+++ Rapi.c
@@ -4,321 +4,6 @@
 #include "AppHead.h"
 #include "rapi.h"
 
-/*!****************************************************************************
- 创建任务
-\author          nanoss
-\date			 2017/10/30
-\brief			 完成任务创建及启动
-\param[in]		 name	 任务名称 输入任务名称
-\param[in]		 priority	 优先级 输入任务的优先级
-\param[in]		 entryPt		函数入口 任务函数入口
-\param[in]		 stackSize	栈大小 任务栈空间
-\return 		 INT32
-*******************************************************************************/
-INT32 RAPI_CreateTask(char * name, int priority, void(*entryPt) (void *), int stackSize)
-{
-#ifdef APP_ACoreOS653
-	return createTaskA653(name, priority, entryPt, stackSize);
-
-#endif
-
-#ifdef APP_VxWorks653
-	return createTaskVX653(name, priority, entryPt, stackSize);
-
-#endif
-
-#ifdef APP_VxWorks5_5
-	return createTaskVX55(name, priority, entryPt, stackSize);
-
-#endif
-
-#ifdef APP_WIN32
-	return createTaskWIN32(name, priority, entryPt, stackSize);
-
-#endif
-}
-
-/*!****************************************************************************
- 任务延时
-\author 		 nanos
-\date			 2017/10/30
-\brief			 用于延迟当前任务指定时间
-\param[in]		 ms	 延迟时间 延时的毫秒数
-\return 		 INT32
-*******************************************************************************/
-INT32 RAPI_TaskDelay(int ms)
-{
-#ifdef APP_ACoreOS653
-	taskDelayA653(ms);
-#endif
-
-#ifdef APP_VxWorks653
-	taskDelayVX653(ms);
-#endif
-
-#ifdef APP_VxWorks5_5
-	taskDelayVX55(ms);
-#endif
-
-#ifdef APP_WIN32
-	taskDelayWIN32(ms);
-#endif
-
-	return 0;
-}
-
-/*!****************************************************************************
- 创建互斥信号量
-\author 		 nanoss
-\date			 2017/10/30
-\brief			 创建互斥信号量
-\return 		 RAPI_SEM_ID
-*******************************************************************************/
-RAPI_SEM_ID RAPI_SemMCreate(void)
-{
-#ifdef APP_ACoreOS653
-	return semMCreateA653();
-
-#endif
-
-#ifdef APP_VxWorks653
-	return semMCreateVX653();
-
-#endif
-
-#ifdef APP_VxWorks5_5
-	return semMCreateVX55();
-
-#endif
-
-#ifdef APP_WIN32
-	return semMCreateWIN32();
-
-#endif
-}
-
-/*!****************************************************************************
- 创建通知信号量
-\author 		 nanoss
-\date			 2017/10/30
-\brief			 创建通知信号量
-\return 		 RAPI_SEM_ID
-*******************************************************************************/
-RAPI_SEM_ID RAPI_SemBCreate(void)
-{
-#ifdef APP_ACoreOS653
-	return semBCreateA653();
-
-#endif
-
-#ifdef APP_VxWorks653
-	return semBCreateVX653();
-
-#endif
-
-#ifdef APP_VxWorks5_5
-	return semBCreateVX55();
-
-#endif
-
-#ifdef APP_WIN32
-	return semBCreateWIN32();
-
-#endif
-}
-
-/*!****************************************************************************
- 信号量获取
-\author 		 nanoss
-\date			 2017/10/30
-\brief			 获取信号量
-\param[in]		 semId	信号量ID 用于输入信号量
-\param[in]		 timeout		超时时间 用于输入超时间
-\return 		 INT32
-\attention 单位：微秒
-*******************************************************************************/
-INT32 RAPI_SemTake(RAPI_SEM_ID semId, int timeout)
-{
-#ifdef APP_ACoreOS653
-	semTakeA653(semId, timeout);
-#endif
-
-#ifdef APP_VxWorks653
-	semTakeVX653(semId, timeout);
-#endif
-
-#ifdef APP_VxWorks5_5
-	semTakeVX55(semId, timeout);
-#endif
-
-#ifdef APP_WIN32
-	semTakeWIN32(semId, timeout);
-#endif
-
-	return 0;
-}
-
-/*!****************************************************************************
- 信号量释放
-\author 		 nanoss
-\date			 2017/10/30
-\brief			 释放信号量
-\param[in]		 semId	信号量ID 用于输入信号量
-\return 		 INT32
-*******************************************************************************/
-void RAPI_SemGive(RAPI_SEM_ID semId)
-{
-#ifdef APP_ACoreOS653
-	semGiveA653(semId);
-#endif
-
-#ifdef APP_VxWorks653
-	semGiveVX653(semId);
-#endif
-
-#ifdef APP_VxWorks5_5
-	semGiveVX55(semId);
-#endif
-
-#ifdef APP_WIN32
-	semGiveWIN32(semId);
-#endif
-
-	return;
-}
-
-/*!****************************************************************************
- 创建消息队列
-\author 		 nanoss
-\date			 2017/10/30
-\brief			 创建消息队列
-\param[in]		 maxQueLen	消息个数 队列中最大消息个数
-\param[in]		 maxMsgLen	消息长度 单个消息内容长度
-\return 		 RAPI_MSG_Q_ID
-*******************************************************************************/
-RAPI_MSG_Q_ID RAPI_MsgQCreate(int maxQueLen, int maxMsgLen)
-{
-#ifdef APP_ACoreOS653
-	return msgQCreateA653(maxQueLen, maxMsgLen);
-
-#endif
-
-#ifdef APP_VxWorks653
-	return msgQCreateVX653(maxQueLen, maxMsgLen);
-
-#endif
-
-#ifdef APP_VxWorks5_5
-	return msgQCreateVX55(maxQueLen, maxMsgLen);
-
-#endif
-
-#ifdef APP_WIN32
-	return msgQCreateWIN32(maxQueLen, maxMsgLen);
-
-#endif
-}
-
-/*!****************************************************************************
- 消息队列入队
-\author 		 nanoss
-\date			 2017/10/30
-\brief			 将消息放入消息队列
-\param[in]		 msgQId	 队列ID 标识消息队列
-\param[in]		 buffer	 数据缓存 消息内容
-\param[in]		 nBytes	 数据长度 数据内容长度
-\param[in]		 timeout		等待时间 最长等待时间
-\return 		 INT32
-\attention timeout,等待时间(ms)
-*******************************************************************************/
-INT32 RAPI_MsgQSend(RAPI_MSG_Q_ID msgQId, char * buffer, UINT32 nBytes, int timeout)
-{
-#ifdef APP_ACoreOS653
-	msgQSendA653(msgQId, buffer, nBytes, timeout);
-#endif
-
-#ifdef APP_VxWorks653
-	msgQSendVX653(msgQId, buffer, nBytes, timeout);
-#endif
-
-#ifdef APP_VxWorks5_5
-	msgQSendVX55(msgQId, buffer, nBytes, timeout);
-#endif
-
-#ifdef APP_WIN32
-	msgQSendWIN32(msgQId, buffer, nBytes, timeout);
-#endif
-
-	return 0;
-}
-
-/*!****************************************************************************
- 消息队列出队
-\author 		 nanoss
-\date			 2017/10/30
-\brief			 将消息取出消息队列
-\param[in]		 msgQId	 队列ID 标识消息队列
-\param[in]		 buffer	 数据缓存 消息内容
-\param[in]		 maxNBytes	数据长度 数据内容长度
-\param[in]		 timeout		等待时间 最长等待时间
-\return 		 INT32
-\attention timeout,等待时间(ms)
-*******************************************************************************/
-INT32 RAPI_MsgQReceive(RAPI_MSG_Q_ID msgQId, char * buffer, UINT32 maxNBytes, int timeout)
-{
-#ifdef APP_ACoreOS653
-	return msgQReceiveA653(msgQId, buffer, maxNBytes, timeout);
-
-#endif
-
-#ifdef APP_VxWorks653
-	return msgQReceiveVX653(msgQId, buffer, maxNBytes, timeout);
-
-#endif
-
-#ifdef APP_VxWorks5_5
-	return msgQReceiveVX55(msgQId, buffer, maxNBytes, timeout);
-
-#endif
-
-#ifdef APP_WIN32
-	return msgQReceiveWIN32(msgQId, buffer, maxNBytes, timeout);
-
-#endif
-
-	return 0;
-}
-
-/*!****************************************************************************
- 系统RTC获取
-\author 		 nanoss
-\date			 2017/10/30
-\brief			 系统RTC获取
-\return 		 UINT64
-\attention 系统RTC(ns)
-*******************************************************************************/
-UINT64 RAPI_RtcGet(void) /*ns*/
-{
-#ifdef APP_ACoreOS653
-	return rtcGetA653();
-
-#endif
-
-#ifdef APP_VxWorks653
-	return rtcGetVX653();
-
-#endif
-
-#ifdef APP_VxWorks5_5
-	return rtcGetVX55();
-
-#endif
-
-#ifdef APP_WIN32
-	return rtcGetWIN32();
-
-#endif
-}
 
 
 #ifdef APP_VxWorks653
@@ -912,4 +597,270 @@ UINT64 rtcGetWIN32(void) /*ns*/
 
 
 #endif
+
+
+
+INT32 RAPI_CreateTask(char * name, int priority, void(*entryPt) (void *), int stackSize)
+{
+
+#ifdef APP_VxWorks653
+	return createTaskVX653(name, priority, entryPt, stackSize);
+
+#endif
+
+#ifdef APP_VxWorks5_5
+	return createTaskVX55(name, priority, entryPt, stackSize);
+
+#endif
+
+#ifdef APP_WIN32
+	return createTaskWIN32(name, priority, entryPt, stackSize);
+
+#endif
+}
+
+/*!****************************************************************************
+ 任务延时
+\author 		 nanos
+\date			 2017/10/30
+\brief			 用于延迟当前任务指定时间
+\param[in]		 ms	 延迟时间 延时的毫秒数
+\return 		 INT32
+*******************************************************************************/
+INT32 RAPI_TaskDelay(int ms)
+{
+
+#ifdef APP_VxWorks653
+	taskDelayVX653(ms);
+#endif
+
+#ifdef APP_VxWorks5_5
+	taskDelayVX55(ms);
+#endif
+
+#ifdef APP_WIN32
+	taskDelayWIN32(ms);
+#endif
+
+	return 0;
+}
+
+/*!****************************************************************************
+ 创建互斥信号量
+\author 		 nanoss
+\date			 2017/10/30
+\brief			 创建互斥信号量
+\return 		 RAPI_SEM_ID
+*******************************************************************************/
+RAPI_SEM_ID RAPI_SemMCreate(void)
+{
+
+#ifdef APP_VxWorks653
+	return semMCreateVX653();
+
+#endif
+
+#ifdef APP_VxWorks5_5
+	return semMCreateVX55();
+
+#endif
+
+#ifdef APP_WIN32
+	return semMCreateWIN32();
+
+#endif
+}
+
+/*!****************************************************************************
+ 创建通知信号量
+\author 		 nanoss
+\date			 2017/10/30
+\brief			 创建通知信号量
+\return 		 RAPI_SEM_ID
+*******************************************************************************/
+RAPI_SEM_ID RAPI_SemBCreate(void)
+{
+
+#ifdef APP_VxWorks653
+	return semBCreateVX653();
+
+#endif
+
+#ifdef APP_VxWorks5_5
+	return semBCreateVX55();
+
+#endif
+
+#ifdef APP_WIN32
+	return semBCreateWIN32();
+
+#endif
+}
+
+/*!****************************************************************************
+ 信号量获取
+\author 		 nanoss
+\date			 2017/10/30
+\brief			 获取信号量
+\param[in]		 semId	信号量ID 用于输入信号量
+\param[in]		 timeout		超时时间 用于输入超时间
+\return 		 INT32
+\attention 单位：微秒
+*******************************************************************************/
+INT32 RAPI_SemTake(RAPI_SEM_ID semId, int timeout)
+{
+#ifdef APP_VxWorks653
+	semTakeVX653(semId, timeout);
+#endif
+
+#ifdef APP_VxWorks5_5
+	semTakeVX55(semId, timeout);
+#endif
+
+#ifdef APP_WIN32
+	semTakeWIN32(semId, timeout);
+#endif
+
+	return 0;
+}
+
+/*!****************************************************************************
+ 信号量释放
+\author 		 nanoss
+\date			 2017/10/30
+\brief			 释放信号量
+\param[in]		 semId	信号量ID 用于输入信号量
+\return 		 INT32
+*******************************************************************************/
+void RAPI_SemGive(RAPI_SEM_ID semId)
+{
+#ifdef APP_VxWorks653
+	semGiveVX653(semId);
+#endif
+
+#ifdef APP_VxWorks5_5
+	semGiveVX55(semId);
+#endif
+
+#ifdef APP_WIN32
+	semGiveWIN32(semId);
+#endif
+
+	return;
+}
+
+/*!****************************************************************************
+ 创建消息队列
+\author 		 nanoss
+\date			 2017/10/30
+\brief			 创建消息队列
+\param[in]		 maxQueLen	消息个数 队列中最大消息个数
+\param[in]		 maxMsgLen	消息长度 单个消息内容长度
+\return 		 RAPI_MSG_Q_ID
+*******************************************************************************/
+RAPI_MSG_Q_ID RAPI_MsgQCreate(int maxQueLen, int maxMsgLen)
+{
+
+#ifdef APP_VxWorks653
+	return msgQCreateVX653(maxQueLen, maxMsgLen);
+
+#endif
+
+#ifdef APP_VxWorks5_5
+	return msgQCreateVX55(maxQueLen, maxMsgLen);
+
+#endif
+
+#ifdef APP_WIN32
+	return msgQCreateWIN32(maxQueLen, maxMsgLen);
+
+#endif
+}
+
+/*!****************************************************************************
+ 消息队列入队
+\author 		 nanoss
+\date			 2017/10/30
+\brief			 将消息放入消息队列
+\param[in]		 msgQId	 队列ID 标识消息队列
+\param[in]		 buffer	 数据缓存 消息内容
+\param[in]		 nBytes	 数据长度 数据内容长度
+\param[in]		 timeout		等待时间 最长等待时间
+\return 		 INT32
+\attention timeout,等待时间(ms)
+*******************************************************************************/
+INT32 RAPI_MsgQSend(RAPI_MSG_Q_ID msgQId, char * buffer, UINT32 nBytes, int timeout)
+{
+#ifdef APP_VxWorks653
+	msgQSendVX653(msgQId, buffer, nBytes, timeout);
+#endif
+
+#ifdef APP_VxWorks5_5
+	msgQSendVX55(msgQId, buffer, nBytes, timeout);
+#endif
+
+#ifdef APP_WIN32
+	msgQSendWIN32(msgQId, buffer, nBytes, timeout);
+#endif
+
+	return 0;
+}
+
+/*!****************************************************************************
+ 消息队列出队
+\author 		 nanoss
+\date			 2017/10/30
+\brief			 将消息取出消息队列
+\param[in]		 msgQId	 队列ID 标识消息队列
+\param[in]		 buffer	 数据缓存 消息内容
+\param[in]		 maxNBytes	数据长度 数据内容长度
+\param[in]		 timeout		等待时间 最长等待时间
+\return 		 INT32
+\attention timeout,等待时间(ms)
+*******************************************************************************/
+INT32 RAPI_MsgQReceive(RAPI_MSG_Q_ID msgQId, char * buffer, UINT32 maxNBytes, int timeout)
+{
+#ifdef APP_VxWorks653
+	return msgQReceiveVX653(msgQId, buffer, maxNBytes, timeout);
+
+#endif
+
+#ifdef APP_VxWorks5_5
+	return msgQReceiveVX55(msgQId, buffer, maxNBytes, timeout);
+
+#endif
+
+#ifdef APP_WIN32
+	return msgQReceiveWIN32(msgQId, buffer, maxNBytes, timeout);
+
+#endif
+
+	return 0;
+}
+
+/*!****************************************************************************
+ 系统RTC获取
+\author 		 nanoss
+\date			 2017/10/30
+\brief			 系统RTC获取
+\return 		 UINT64
+\attention 系统RTC(ns)
+*******************************************************************************/
+UINT64 RAPI_RtcGet(void) /*ns*/
+{
+#ifdef APP_VxWorks653
+	return rtcGetVX653();
+
+#endif
+
+#ifdef APP_VxWorks5_5
+	return rtcGetVX55();
+
+#endif
+
+#ifdef APP_WIN32
+	return rtcGetWIN32();
+
+#endif
+}
 
